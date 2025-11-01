2a4a885bf8b89ba20c51afd6d26c7350ad729f2c
Phireyaanth Poobalaraj <ppoobalaraj@gmail.com>
2025-10-31 23:02:03 -0400

    AXIOS BE GONE

--- client/src/auth/requests/index.js
+++ client/src/auth/requests/index.js
@@ -1,50 +1,80 @@
 /*
     This is our http api for all things auth, which we use to 
     send authorization requests to our back-end API. Note we`re 
-    using the Axios library for doing this, which is an easy to 
+    using the  library for doing this, which is an easy to 
     use AJAX-based library. We could (and maybe should) use Fetch, 
-    which is a native (to browsers) standard, but Axios is easier
+    which is a native (to browsers) standard, but  is easier
     to use when sending JSON back and forth and it`s a Promise-
     based API which helps a lot with asynchronous communication.
     
     @author McKilla Gorilla
 */
+// client/src/auth/requests/index.js
+const BASE_URL = 'http://localhost:4000/auth';
 
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost:4000/auth',
-})
+// GET /loggedIn
+export const getLoggedIn = async () => {
+  const res = await fetch(`${BASE_URL}/loggedIn/`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
 
-// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
-// REQUEST METHOD (like get) AND PATH (like /register). SOME ALSO
-// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
-// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
-// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
-// CUSTOM FILTERS FOR QUERIES
+// POST /login
+export const loginUser = async (email, password) => {
+  const res = await fetch(`${BASE_URL}/login/`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
 
-export const getLoggedIn = () => api.get(`/loggedIn/`);
-export const loginUser = (email, password) => {
-    return api.post(`/login/`, {
-        email : email,
-        password : password
-    })
-}
-export const logoutUser = () => api.get(`/logout/`)
-export const registerUser = (firstName, lastName, email, password, passwordVerify) => {
-    return api.post(`/register/`, {
-        firstName : firstName,
-        lastName : lastName,
-        email : email,
-        password : password,
-        passwordVerify : passwordVerify
-    })
-}
-const apis = {
-    getLoggedIn,
-    registerUser,
-    loginUser,
-    logoutUser
-}
+// GET /logout
+export const logoutUser = async () => {
+  const res = await fetch(`${BASE_URL}/logout/`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
 
-export default apis
+// POST /register
+export const registerUser = async (firstName, lastName, email, password, passwordVerify) => {
+  const res = await fetch(`${BASE_URL}/register/`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ firstName, lastName, email, password, passwordVerify }),
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
+
+const apis = { getLoggedIn, registerUser, loginUser, logoutUser };
+export default apis;
--- client/src/store/requests/index.js
+++ client/src/store/requests/index.js
@@ -1,51 +1,102 @@
 /*
     This is our http api, which we use to send requests to
-    our back-end API. Note we`re using the Axios library
+    our back-end API. Note we`re using the  library
     for doing this, which is an easy to use AJAX-based
     library. We could (and maybe should) use Fetch, which
-    is a native (to browsers) standard, but Axios is easier
+    is a native (to browsers) standard, but  is easier
     to use when sending JSON back and forth and it`s a Promise-
     based API which helps a lot with asynchronous communication.
     
     @author McKilla Gorilla
 */
 
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost:4000/store',
-})
+// client/src/store/requests/index.js
+const BASE_URL = 'http://localhost:4000/store';
 
-// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
-// REQUEST METHOD (like get) AND PATH (like /top5list). SOME ALSO
-// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
-// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
-// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
-// CUSTOM FILTERS FOR QUERIES
-export const createPlaylist = (newListName, newSongs, userEmail) => {
-    return api.post(`/playlist/`, {
-        // SPECIFY THE PAYLOAD
-        name: newListName,
-        songs: newSongs,
-        ownerEmail: userEmail
-    })
-}
-export const deletePlaylistById = (id) => api.delete(`/playlist/${id}`)
-export const getPlaylistById = (id) => api.get(`/playlist/${id}`)
-export const getPlaylistPairs = () => api.get(`/playlistpairs/`)
-export const updatePlaylistById = (id, playlist) => {
-    return api.put(`/playlist/${id}`, {
-        // SPECIFY THE PAYLOAD
-        playlist : playlist
-    })
-}
+// POST /playlist
+export const createPlaylist = async (newListName, newSongs, userEmail) => {
+  const res = await fetch(`${BASE_URL}/playlist/`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: newListName, songs: newSongs, ownerEmail: userEmail }),
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
+
+// DELETE /playlist/:id
+export const deletePlaylistById = async (id) => {
+  const res = await fetch(`${BASE_URL}/playlist/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
+
+// GET /playlist/:id
+export const getPlaylistById = async (id) => {
+  const res = await fetch(`${BASE_URL}/playlist/${id}`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
+
+// GET /playlistpairs
+export const getPlaylistPairs = async () => {
+  const res = await fetch(`${BASE_URL}/playlistpairs/`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
+
+// PUT /playlist/:id
+export const updatePlaylistById = async (id, playlist) => {
+  const res = await fetch(`${BASE_URL}/playlist/${id}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ playlist }),
+  });
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) {
+    const err = new Error(data?.message || res.statusText);
+    err.response = { status: res.status, data };
+    throw err;
+  }
+  return { data, status: res.status };
+};
 
 const apis = {
-    createPlaylist,
-    deletePlaylistById,
-    getPlaylistById,
-    getPlaylistPairs,
-    updatePlaylistById
-}
-
-export default apis
+  createPlaylist,
+  deletePlaylistById,
+  getPlaylistById,
+  getPlaylistPairs,
+  updatePlaylistById,
+};
+export default apis;
